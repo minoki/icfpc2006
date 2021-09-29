13dc9062953324c2aeaee5398464b8c49774dffb
ARATA Mizuki <minorinoki@gmail.com>
2021-09-29 11:43:14 +0900

    Fix UNLIKELY

--- run.c
+++ run.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #if defined(__GNUC__)
 #define LIKELY(x) __builtin_expect(!!(x), 1)
-#define UNLIKELY(x) __builtin_expect(!!(x), 1)
+#define UNLIKELY(x) __builtin_expect(!!(x), 0)
 #else
 #define LIKELY(x) (x)
 #define UNLIKELY(x) (x)
