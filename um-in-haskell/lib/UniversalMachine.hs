ac07ffbb1307fef2c0a120fe2837ff214facfa8b
ARATA Mizuki <minorinoki@gmail.com>
2021-10-16 23:12:46 +0900

    UM in Haskell: Faster VM

--- um-in-haskell/lib/UniversalMachine.hs
+++ um-in-haskell/lib/UniversalMachine.hs
@@ -2,6 +2,7 @@
 module UniversalMachine where
 import           Control.Monad
 import           Control.Monad.Primitive
+import           Control.Monad.ST            hiding (runST)
 import           Data.Bits
 import qualified Data.ByteString             as BS
 import qualified Data.Vector.Mutable         as VM
@@ -30,10 +31,8 @@ initialState !program = do
   registers <- UM.replicate 8 0
   return (State { pc = 0, arrays = arrays, registers = registers, freelist = [] })
 
-{-# INLINE run #-}
-{-# SPECIALIZE run :: IO (Maybe Word8) -> (Word8 -> IO ()) -> State RealWorld -> IO (State RealWorld) #-}
-run :: PrimMonad m => m (Maybe Word8) -> (Word8 -> m ()) -> State (PrimState m) -> m (State (PrimState m))
-run getChar putChar !state@(State !pc !arrays !registers !freelist) = do
+runST :: State s -> ST s (State s)
+runST !state@(State !pc !arrays !registers !freelist) = do
   !array0 <- VM.read arrays 0
   let readReg !a = UM.unsafeRead registers a
       writeReg !a !v = UM.unsafeWrite registers a v
@@ -83,34 +82,27 @@ run getChar putChar !state@(State !pc !arrays !registers !freelist) = do
                writeReg a (complement (x .&. y))
                go (pc + 1)
           7 -> -- Halt
-            return state
+            return state { pc = pc }
           8 -> -- Allocation
             do capacity <- readReg c
                newArr <- UM.replicate (fromIntegral capacity) 0
-               (!i,arrays',freelist') <- case freelist of
-                                           [] -> do let !n = VM.length arrays
-                                                    arrays' <- VM.grow arrays 1
-                                                    VM.write arrays' n newArr
-                                                    return (n, arrays', [])
-                                           x:xs -> do VM.write arrays x newArr
-                                                      return (x, arrays, xs)
+               (i,arrays',freelist') <- case freelist of
+                                          [] -> do let !n = VM.length arrays
+                                                   arrays' <- VM.grow arrays 1
+                                                   VM.write arrays' n newArr
+                                                   return (n, arrays', [])
+                                          x:xs -> do VM.write arrays x newArr
+                                                     return (x, arrays, xs)
                writeReg b (fromIntegral i)
-               run getChar putChar (State (pc + 1) arrays' registers freelist')
+               runST (State (pc + 1) arrays' registers freelist')
           9 -> -- Abandonment
             do i <- readReg c
                VM.write arrays (fromIntegral i) undefined
-               run getChar putChar (State (pc + 1) arrays registers (fromIntegral i : freelist))
+               runST (State (pc + 1) arrays registers (fromIntegral i : freelist))
           10 -> -- Output
-            do v <- readReg c
-               -- assert: v <= 255
-               putChar (fromIntegral v)
-               go (pc + 1)
+            return state { pc = pc }
           11 -> -- Input
-            do v <- getChar
-               writeReg c $! case v of
-                               Nothing -> 0xFFFFFFFF
-                               Just x  -> fromIntegral x
-               go (pc + 1)
+            return state { pc = pc }
           12 -> -- Load Program
             do i <- readReg b
                pc' <- fromIntegral <$> readReg c
@@ -118,8 +110,8 @@ run getChar putChar !state@(State !pc !arrays !registers !freelist) = do
                  arr_i <- VM.read arrays (fromIntegral i)
                  arr_0 <- UM.clone arr_i
                  VM.write arrays 0 arr_0
-                 run getChar putChar (State pc' arrays registers freelist) -- replace 0 array
-                 else
+                 runST (State pc' arrays registers freelist) -- replace 0 array
+                 else do
                  go pc'
           13 -> -- Orthography
             do let !a = fromIntegral ((op `unsafeShiftR` 25) .&. 7)
@@ -128,3 +120,101 @@ run getChar putChar !state@(State !pc !arrays !registers !freelist) = do
                go (pc + 1)
           _ -> error "Invalid Instruction"
   go pc
+
+{-# INLINE run #-}
+{-# SPECIALIZE run :: IO (Maybe Word8) -> (Word8 -> IO ()) -> State RealWorld -> IO (State RealWorld) #-}
+run :: PrimMonad m => m (Maybe Word8) -> (Word8 -> m ()) -> State (PrimState m) -> m (State (PrimState m))
+run getChar putChar state0 = do
+  state@(State !pc !arrays !registers !freelist) <- stToPrim $ runST state0
+  !array0 <- VM.read arrays 0
+  let readReg !a = UM.unsafeRead registers a
+      writeReg !a !v = UM.unsafeWrite registers a v
+  !op <- UM.read array0 pc
+  let a, b, c :: Int
+      a = fromIntegral ((op `unsafeShiftR` 6) .&. 7)
+      b = fromIntegral ((op `unsafeShiftR` 3) .&. 7)
+      c = fromIntegral (op .&. 7)
+  case op `unsafeShiftR` 28 of
+    0 -> -- Conditional Move
+      do v <- readReg c
+         when (v /= 0) $ readReg b >>= writeReg a
+         run getChar putChar (State (pc + 1) arrays registers freelist)
+    1 -> -- Array Index
+      do i <- readReg b
+         !arr_i <- VM.read arrays (fromIntegral i)
+         j <- readReg c
+         v <- UM.read arr_i (fromIntegral j)
+         writeReg a v
+         run getChar putChar (State (pc + 1) arrays registers freelist)
+    2 -> -- Array Amendment
+      do i <- readReg a
+         !arr_i <- VM.read arrays (fromIntegral i)
+         j <- readReg b
+         v <- readReg c
+         UM.write arr_i (fromIntegral j) v
+         run getChar putChar (State (pc + 1) arrays registers freelist)
+    3 -> -- Addition
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (x + y)
+         run getChar putChar (State (pc + 1) arrays registers freelist)
+    4 -> -- Multiplication
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (x * y)
+         run getChar putChar (State (pc + 1) arrays registers freelist)
+    5 -> -- Division
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (x `quot` y)
+         run getChar putChar (State (pc + 1) arrays registers freelist)
+    6 -> -- Not-And
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (complement (x .&. y))
+         run getChar putChar (State (pc + 1) arrays registers freelist)
+    7 -> -- Halt
+      return state
+    8 -> -- Allocation
+      do capacity <- readReg c
+         newArr <- UM.replicate (fromIntegral capacity) 0
+         (i,arrays',freelist') <- case freelist of
+                                    [] -> do let !n = VM.length arrays
+                                             arrays' <- VM.grow arrays 1
+                                             VM.write arrays' n newArr
+                                             return (n, arrays', [])
+                                    x:xs -> do VM.write arrays x newArr
+                                               return (x, arrays, xs)
+         writeReg b (fromIntegral i)
+         run getChar putChar (State (pc + 1) arrays' registers freelist')
+    9 -> -- Abandonment
+      do i <- readReg c
+         VM.write arrays (fromIntegral i) undefined
+         run getChar putChar (State (pc + 1) arrays registers (fromIntegral i : freelist))
+    10 -> -- Output
+      do v <- readReg c
+         -- assert: v <= 255
+         putChar (fromIntegral v)
+         run getChar putChar (State (pc + 1) arrays registers freelist)
+    11 -> -- Input
+      do v <- getChar
+         writeReg c $! case v of
+                         Nothing -> 0xFFFFFFFF
+                         Just x  -> fromIntegral x
+         run getChar putChar (State (pc + 1) arrays registers freelist)
+    12 -> -- Load Program
+      do i <- readReg b
+         pc' <- fromIntegral <$> readReg c
+         if i /= 0 then do
+           arr_i <- VM.read arrays (fromIntegral i)
+           arr_0 <- UM.clone arr_i
+           VM.write arrays 0 arr_0
+           run getChar putChar (State pc' arrays registers freelist) -- replace 0 array
+           else
+           run getChar putChar (State pc' arrays registers freelist)
+    13 -> -- Orthography
+      do let !a = fromIntegral ((op `unsafeShiftR` 25) .&. 7)
+             !value = op .&. 0x1FFFFFF
+         writeReg a value
+         run getChar putChar (State (pc + 1) arrays registers freelist)
+    _ -> error "Invalid Instruction"
