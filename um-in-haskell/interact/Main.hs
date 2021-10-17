4e5fa0378bffe13d318d3aecc315c2b40ffad21e
ARATA Mizuki <minorinoki@gmail.com>
2021-10-17 11:45:32 +0900

    UM in Haskell: Add runSemiIO variant

--- um-in-haskell/interact/Main.hs
+++ um-in-haskell/interact/Main.hs
@@ -7,6 +7,8 @@ import           Control.Monad.Trans.Cont
 import           Data.Bits
 import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Char8      as BSC
+import           Data.IORef
+import qualified Data.List                  as List
 import           Data.Vector.Unboxed        as U
 import           Data.Word
 import           Prelude                    hiding (getChar, putChar)
@@ -14,29 +16,7 @@ import           System.Environment
 import           System.IO                  hiding (getChar, putChar)
 import           UniversalMachine
 
-{-
-getChar :: IO (Maybe Word8)
-getChar = do result <- BS.hGet stdin 1
-             return $! if BS.null result then
-                         Nothing
-                       else
-                         Just $! (BS.index result 0)
-
-putChar :: Word8 -> IO ()
-putChar c = BS.putStr (BS.singleton c)
-
-type M a = StateT {- input supply -} [Word8] (AccumT {- output -} [Word8] (ContT {- message -} (Maybe [Word8]) IO)) a
-
-getChar :: M (Maybe Word8)
-getChar = do supply <- get
-             case supply of
-               x:xs -> do put xs
-                          return (Just x)
-               [] -> return Nothing
--}
-
--- newtype MyCont = MyCont (Maybe Word8 -> ContT (Either BS.ByteString (BS.ByteString, MyCont)) IO (Either BS.ByteString (BS.ByteString, MyCont)))
-newtype MyCont = MyCont (Maybe Word8 -> AccumT BS.ByteString (ContT (Maybe MyCont, BS.ByteString) IO) (Maybe MyCont, BS.ByteString))
+newtype MyCont = MyCont (BS.ByteString -> ContT (Maybe MyCont) IO (Maybe MyCont))
 
 main :: IO ()
 main = do args <- getArgs
@@ -44,30 +24,29 @@ main = do args <- getArgs
             [] -> putStrLn "Hello, Haskell!"
             filename:_ -> do programBE <- BS.readFile filename
                              state0 <- initialState (programFromByteString programBE)
-                             -- let initialSupply = BS.unpack (BSC.pack "guest\nexit\n")
-                             r <- evalContT $ do
-                               liftIO $ putStrLn "start"
-                               flip runAccumT BS.empty $ liftCallCC callCC $ \(cont :: Maybe MyCont -> AccumT BS.ByteString (ContT (Maybe MyCont, BS.ByteString) IO) (Maybe Word8)) -> do
-                                 let getChar = liftCallCC callCC $ \(cont' :: Maybe Word8 -> AccumT BS.ByteString (ContT (Maybe MyCont, BS.ByteString) IO) (Maybe MyCont, BS.ByteString)) -> do
-                                       cont (Just (MyCont cont'))
-                                 let putChar c = add (BS.singleton c)
-                                 run getChar putChar state0
-                                 return Nothing
-                             liftIO $ putStrLn "returned"
-                             let go :: (Maybe MyCont, BS.ByteString) -> [Word8] -> IO ()
-                                 go (Nothing, m) chars = do
-                                   BSC.putStr m
-                                   BSC.putStrLn "---"
-                                   BSC.putStrLn ("Unconsumed input:" <> BS.pack chars)
-                                   return ()
-                                 go (Just (MyCont cont), m) [] = do
-                                   BSC.putStr m
-                                   BSC.putStrLn "---"
-                                   BSC.putStrLn "Input exhausted"
-                                   return ()
-                                 go (Just (MyCont cont), m) (x:xs) = do
-                                   BSC.putStrLn m
-                                   r <- evalContT $ evalAccumT (cont (Just x)) BS.empty
-                                   go r xs
-                             go r (BS.unpack "guest\nexit\n")
-                             return ()
+                             outputBuf <- newIORef []
+                             cont <- evalContT $ callCC $ \(cont :: Maybe MyCont -> ContT (Maybe MyCont) IO BS.ByteString) -> do
+                               let getStr = callCC $ \(cont' :: BS.ByteString -> ContT (Maybe MyCont) IO (Maybe MyCont)) -> do
+                                     cont (Just (MyCont cont'))
+                               let putChar c = modifyIORef' outputBuf (c:)
+                               runSemiIO getStr putChar BS.empty state0
+                               return Nothing
+                             let collectOutput = do xs <- readIORef outputBuf
+                                                    writeIORef outputBuf []
+                                                    return (BS.pack (List.reverse xs))
+                             contV <- newIORef cont
+                             let interact input = do cont <- readIORef contV
+                                                     case cont of
+                                                       Nothing -> do putStrLn "VM halted"
+                                                                     return BS.empty
+                                                       Just (MyCont cont) -> do cont' <- evalContT $ cont input
+                                                                                writeIORef contV cont'
+                                                                                collectOutput
+                             collectOutput >>= BSC.putStr
+                             putStrLn "---"
+                             output2 <- interact "guest\n"
+                             BSC.putStr output2
+                             putStrLn "---"
+                             output3 <- interact "exit\n"
+                             BSC.putStr output3
+                             putStrLn "---"
--- um-in-haskell/lib/UniversalMachine.hs
+++ um-in-haskell/lib/UniversalMachine.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE TypeFamilies #-}
 module UniversalMachine where
 import           Control.Monad
 import           Control.Monad.Primitive
@@ -122,8 +123,8 @@ runST !state@(State !pc !arrays !registers !freelist) = do
   go pc
 
 {-# INLINE run #-}
-{-# SPECIALIZE run :: IO (Maybe Word8) -> (Word8 -> IO ()) -> State RealWorld -> IO (State RealWorld) #-}
-run :: PrimMonad m => m (Maybe Word8) -> (Word8 -> m ()) -> State (PrimState m) -> m (State (PrimState m))
+{-# SPECIALIZE run :: IO (Maybe Word8) -> (Word8 -> IO ()) -> State RealWorld -> IO () #-}
+run :: PrimMonad m => m (Maybe Word8) -> (Word8 -> m ()) -> State (PrimState m) -> m ()
 run getChar putChar state0 = do
   state@(State !pc !arrays !registers !freelist) <- stToPrim $ runST state0
   !array0 <- VM.read arrays 0
@@ -174,7 +175,7 @@ run getChar putChar state0 = do
          writeReg a (complement (x .&. y))
          run getChar putChar (State (pc + 1) arrays registers freelist)
     7 -> -- Halt
-      return state
+      return ()
     8 -> -- Allocation
       do capacity <- readReg c
          newArr <- UM.replicate (fromIntegral capacity) 0
@@ -217,4 +218,200 @@ run getChar putChar state0 = do
              !value = op .&. 0x1FFFFFF
          writeReg a value
          run getChar putChar (State (pc + 1) arrays registers freelist)
+    _ -> error "Invalid Instruction"
+
+runSemiIO' :: (Word8 -> IO ()) -> BS.ByteString -> State RealWorld -> IO (State RealWorld, BS.ByteString)
+runSemiIO' putChar inputBuf state0 = do
+  state@(State !pc !arrays !registers !freelist) <- stToIO $ runST state0
+  !array0 <- VM.read arrays 0
+  let readReg !a = UM.unsafeRead registers a
+      writeReg !a !v = UM.unsafeWrite registers a v
+  !op <- UM.read array0 pc
+  let a, b, c :: Int
+      a = fromIntegral ((op `unsafeShiftR` 6) .&. 7)
+      b = fromIntegral ((op `unsafeShiftR` 3) .&. 7)
+      c = fromIntegral (op .&. 7)
+  case op `unsafeShiftR` 28 of
+    0 -> -- Conditional Move
+      do v <- readReg c
+         when (v /= 0) $ readReg b >>= writeReg a
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays registers freelist)
+    1 -> -- Array Index
+      do i <- readReg b
+         !arr_i <- VM.read arrays (fromIntegral i)
+         j <- readReg c
+         v <- UM.read arr_i (fromIntegral j)
+         writeReg a v
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays registers freelist)
+    2 -> -- Array Amendment
+      do i <- readReg a
+         !arr_i <- VM.read arrays (fromIntegral i)
+         j <- readReg b
+         v <- readReg c
+         UM.write arr_i (fromIntegral j) v
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays registers freelist)
+    3 -> -- Addition
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (x + y)
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays registers freelist)
+    4 -> -- Multiplication
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (x * y)
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays registers freelist)
+    5 -> -- Division
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (x `quot` y)
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays registers freelist)
+    6 -> -- Not-And
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (complement (x .&. y))
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays registers freelist)
+    7 -> -- Halt
+      return (state, inputBuf)
+    8 -> -- Allocation
+      do capacity <- readReg c
+         newArr <- UM.replicate (fromIntegral capacity) 0
+         (i,arrays',freelist') <- case freelist of
+                                    [] -> do let !n = VM.length arrays
+                                             arrays' <- VM.grow arrays 1
+                                             VM.write arrays' n newArr
+                                             return (n, arrays', [])
+                                    x:xs -> do VM.write arrays x newArr
+                                               return (x, arrays, xs)
+         writeReg b (fromIntegral i)
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays' registers freelist')
+    9 -> -- Abandonment
+      do i <- readReg c
+         VM.write arrays (fromIntegral i) undefined
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays registers (fromIntegral i : freelist))
+    10 -> -- Output
+      do v <- readReg c
+         -- assert: v <= 255
+         putChar (fromIntegral v)
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays registers freelist)
+    11 -> -- Input
+      case BS.uncons inputBuf of
+        Nothing -> return (state, inputBuf)
+        Just (x,xs) -> do writeReg c $ fromIntegral x
+                          runSemiIO' putChar xs (State (pc + 1) arrays registers freelist)
+    12 -> -- Load Program
+      do i <- readReg b
+         pc' <- fromIntegral <$> readReg c
+         if i /= 0 then do
+           arr_i <- VM.read arrays (fromIntegral i)
+           arr_0 <- UM.clone arr_i
+           VM.write arrays 0 arr_0
+           runSemiIO' putChar inputBuf (State pc' arrays registers freelist) -- replace 0 array
+           else
+           runSemiIO' putChar inputBuf (State pc' arrays registers freelist)
+    13 -> -- Orthography
+      do let !a = fromIntegral ((op `unsafeShiftR` 25) .&. 7)
+             !value = op .&. 0x1FFFFFF
+         writeReg a value
+         runSemiIO' putChar inputBuf (State (pc + 1) arrays registers freelist)
+    _ -> error "Invalid Instruction"
+
+runSemiIO :: (PrimMonad m, PrimState m ~ RealWorld) => m BS.ByteString -> (Word8 -> IO ()) -> BS.ByteString -> State RealWorld -> m ()
+runSemiIO getStr putChar inputBuf0 state0 = do
+  (state@(State !pc !arrays !registers !freelist), inputBuf) <- ioToPrim $ runSemiIO' putChar inputBuf0 state0
+  !array0 <- VM.read arrays 0
+  let readReg !a = UM.unsafeRead registers a
+      writeReg !a !v = UM.unsafeWrite registers a v
+  !op <- UM.read array0 pc
+  let a, b, c :: Int
+      a = fromIntegral ((op `unsafeShiftR` 6) .&. 7)
+      b = fromIntegral ((op `unsafeShiftR` 3) .&. 7)
+      c = fromIntegral (op .&. 7)
+  case op `unsafeShiftR` 28 of
+    0 -> -- Conditional Move
+      do v <- readReg c
+         when (v /= 0) $ readReg b >>= writeReg a
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays registers freelist)
+    1 -> -- Array Index
+      do i <- readReg b
+         !arr_i <- VM.read arrays (fromIntegral i)
+         j <- readReg c
+         v <- UM.read arr_i (fromIntegral j)
+         writeReg a v
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays registers freelist)
+    2 -> -- Array Amendment
+      do i <- readReg a
+         !arr_i <- VM.read arrays (fromIntegral i)
+         j <- readReg b
+         v <- readReg c
+         UM.write arr_i (fromIntegral j) v
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays registers freelist)
+    3 -> -- Addition
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (x + y)
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays registers freelist)
+    4 -> -- Multiplication
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (x * y)
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays registers freelist)
+    5 -> -- Division
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (x `quot` y)
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays registers freelist)
+    6 -> -- Not-And
+      do x <- readReg b
+         y <- readReg c
+         writeReg a (complement (x .&. y))
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays registers freelist)
+    7 -> -- Halt
+      return ()
+    8 -> -- Allocation
+      do capacity <- readReg c
+         newArr <- UM.replicate (fromIntegral capacity) 0
+         (i,arrays',freelist') <- case freelist of
+                                    [] -> do let !n = VM.length arrays
+                                             arrays' <- VM.grow arrays 1
+                                             VM.write arrays' n newArr
+                                             return (n, arrays', [])
+                                    x:xs -> do VM.write arrays x newArr
+                                               return (x, arrays, xs)
+         writeReg b (fromIntegral i)
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays' registers freelist')
+    9 -> -- Abandonment
+      do i <- readReg c
+         VM.write arrays (fromIntegral i) undefined
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays registers (fromIntegral i : freelist))
+    10 -> -- Output
+      do v <- readReg c
+         -- assert: v <= 255
+         ioToPrim $ putChar (fromIntegral v)
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays registers freelist)
+    11 -> -- Input
+      case BS.uncons inputBuf of
+        Nothing -> do s <- getStr
+                      newInputBuf <- case BS.uncons s of
+                                       Nothing -> do writeReg c 0xFFFFFFFF
+                                                     return BS.empty
+                                       Just (x,xs) -> do writeReg c (fromIntegral x)
+                                                         return xs
+                      runSemiIO getStr putChar newInputBuf (State (pc + 1) arrays registers freelist)
+        Just (x,xs) -> do writeReg c $ fromIntegral x
+                          runSemiIO getStr putChar xs (State (pc + 1) arrays registers freelist)
+    12 -> -- Load Program
+      do i <- readReg b
+         pc' <- fromIntegral <$> readReg c
+         if i /= 0 then do
+           arr_i <- VM.read arrays (fromIntegral i)
+           arr_0 <- UM.clone arr_i
+           VM.write arrays 0 arr_0
+           runSemiIO getStr putChar inputBuf (State pc' arrays registers freelist) -- replace 0 array
+           else
+           runSemiIO getStr putChar inputBuf (State pc' arrays registers freelist)
+    13 -> -- Orthography
+      do let !a = fromIntegral ((op `unsafeShiftR` 25) .&. 7)
+             !value = op .&. 0x1FFFFFF
+         writeReg a value
+         runSemiIO getStr putChar inputBuf (State (pc + 1) arrays registers freelist)
     _ -> error "Invalid Instruction"
