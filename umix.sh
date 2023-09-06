67f1de24038d76d9c2938302ee39c10fcfa216f8
ARATA Mizuki <minorinoki@gmail.com>
2023-09-06 09:58:52 +0900

    Make umix_balance.sh pwd-independent

--- umix.sh
+++ umix.sh
@@ -1,6 +1,10 @@
 if [ -x aarch64/run_jit ]; then
     RUN="$(dirname $0)/aarch64/run_jit"
 else
-    RUN="$(dirname $0)/run"
+    if [ -x x86_64/run_jit ]; then
+        RUN="$(dirname $0)/x86_64/run_jit"
+    else
+        RUN="$(dirname $0)/run"
+    fi
 fi
 ${RUN} "$(dirname $0)/umix.um"
--- umix_balance.sh
+++ umix_balance.sh
@@ -1,6 +1,10 @@
 if [ -x aarch64/run_jit ]; then
-    RUN=aarch64/run_jit
+    RUN="$(dirname $0)/aarch64/run_jit"
 else
-    RUN=run
+    if [ -x x86_64/run_jit ]; then
+        RUN="$(dirname $0)/x86_64/run_jit"
+    else
+        RUN="$(dirname $0)/run"
+    fi
 fi
-${RUN} --discard-initial-output --input yang --input U+262F umix.um
+${RUN} --discard-initial-output --input yang --input U+262F "$(dirname $0)/umix.um"
